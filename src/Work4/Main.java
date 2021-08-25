ce2ceee0ebae20ca6821892bd5bfc105dcbf0eae
Kiril Rumyantsev <kiril.rumyantsev228@gmail.com>
2021-08-25 15:57:48 +0300

    Added transponding

--- src/Work4/Main.java
+++ src/Work4/Main.java
@@ -3,7 +3,11 @@ package Work4;
 public class Main {
     public static void main(String[] args) {
         MatrixCalculator matrixCalculator = new MatrixCalculator();
+        matrixCalculator.fillMatrix();
         matrixCalculator.printMatrix();
         matrixCalculator.printResult();
+        var matrixTrans = new MatrixTransponder();
+        matrixTrans.transMatrix(matrixCalculator.getMatrix());
+        matrixCalculator.printMatrix();
     }
 }
---
+++ src/Work4/MatrixTransponder.java
@@ -0,0 +1,13 @@
+package Work4;
+
+public class MatrixTransponder {
+    public void transMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
